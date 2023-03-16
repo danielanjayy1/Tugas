7ef63f66d0d65ff2c2a9f65184dc4c7c3cdce149
Daniel Anju Saputra Simbolon <danielanjayy12@gmail.com>
2023-03-17 06:42:32 +0700

    uploud Tugas

---
+++ sidemenu/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sidemenu-daniel',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
